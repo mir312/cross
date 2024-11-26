fb73eeead3ae830675b9bfa5e9ea521c49e343b6
mir312 <dakievameerim0@gmail.com>
2024-11-27 00:25:21 +0600

    add styles

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -58,6 +58,227 @@ import img1 from '../../img/img1.jpg'
 
 </div>
 
+
+<div className="card">
+
+      <img src={img1} alt="" className="card_img" />
+      <div className="bord"><i className="heart"></i></div>
+
+      <p className="tit_desc">Мужские Кроссовки Under Armour Curry 8</p>
+
+      <div className="price flex">
+        <div className="price_num">
+          <p className="price_txt">ЦEНА:</p>
+          <span className='number'>12 999 руб.</span>
+        </div>
+
+
+<div className="bord_p">
+  <i className="plus"></i>
+</div>
+      </div>
+      
+    </div>
+
+
+    <div className="card">
+
+      <img src={img1} alt="" className="card_img" />
+      <div className="bord"><i className="heart"></i></div>
+
+      <p className="tit_desc">Мужские Кроссовки Under Armour Curry 8</p>
+
+      <div className="price flex">
+        <div className="price_num">
+          <p className="price_txt">ЦEНА:</p>
+          <span className='number'>12 999 руб.</span>
+        </div>
+
+
+<div className="bord_p">
+  <i className="plus"></i>
+</div>
+      </div>
+      
+    </div>
+
+
+
+    <div className="card">
+
+      <img src={img1} alt="" className="card_img" />
+      <div className="bord"><i className="heart"></i></div>
+
+      <p className="tit_desc">Мужские Кроссовки Under Armour Curry 8</p>
+
+      <div className="price flex">
+        <div className="price_num">
+          <p className="price_txt">ЦEНА:</p>
+          <span className='number'>12 999 руб.</span>
+        </div>
+
+
+<div className="bord_p">
+  <i className="plus"></i>
+</div>
+      </div>
+      
+    </div>
+
+
+
+    <div className="card">
+
+      <img src={img1} alt="" className="card_img" />
+      <div className="bord"><i className="heart"></i></div>
+
+      <p className="tit_desc">Мужские Кроссовки Under Armour Curry 8</p>
+
+      <div className="price flex">
+        <div className="price_num">
+          <p className="price_txt">ЦEНА:</p>
+          <span className='number'>12 999 руб.</span>
+        </div>
+
+
+<div className="bord_p">
+  <i className="plus"></i>
+</div>
+      </div>
+      
+    </div>
+
+
+    <div className="card">
+
+      <img src={img1} alt="" className="card_img" />
+      <div className="bord"><i className="heart"></i></div>
+
+      <p className="tit_desc">Мужские Кроссовки Under Armour Curry 8</p>
+
+      <div className="price flex">
+        <div className="price_num">
+          <p className="price_txt">ЦEНА:</p>
+          <span className='number'>12 999 руб.</span>
+        </div>
+
+
+<div className="bord_p">
+  <i className="plus"></i>
+</div>
+      </div>
+      
+    </div>
+
+
+
+    <div className="card">
+
+      <img src={img1} alt="" className="card_img" />
+      <div className="bord"><i className="heart"></i></div>
+
+      <p className="tit_desc">Мужские Кроссовки Under Armour Curry 8</p>
+
+      <div className="price flex">
+        <div className="price_num">
+          <p className="price_txt">ЦEНА:</p>
+          <span className='number'>12 999 руб.</span>
+        </div>
+
+
+<div className="bord_p">
+  <i className="plus"></i>
+</div>
+      </div>
+      
+    </div>
+
+    <div className="card">
+
+      <img src={img1} alt="" className="card_img" />
+      <div className="bord"><i className="heart"></i></div>
+
+      <p className="tit_desc">Мужские Кроссовки Under Armour Curry 8</p>
+
+      <div className="price flex">
+        <div className="price_num">
+          <p className="price_txt">ЦEНА:</p>
+          <span className='number'>12 999 руб.</span>
+        </div>
+
+
+<div className="bord_p">
+  <i className="plus"></i>
+</div>
+      </div>
+      
+    </div>
+
+
+    <div className="card">
+
+      <img src={img1} alt="" className="card_img" />
+      <div className="bord"><i className="heart"></i></div>
+
+      <p className="tit_desc">Мужские Кроссовки Under Armour Curry 8</p>
+
+      <div className="price flex">
+        <div className="price_num">
+          <p className="price_txt">ЦEНА:</p>
+          <span className='number'>12 999 руб.</span>
+        </div>
+
+
+<div className="bord_p">
+  <i className="plus"></i>
+</div>
+      </div>
+      
+    </div>
+
+    <div className="card">
+
+      <img src={img1} alt="" className="card_img" />
+      <div className="bord"><i className="heart"></i></div>
+
+      <p className="tit_desc">Мужские Кроссовки Under Armour Curry 8</p>
+
+      <div className="price flex">
+        <div className="price_num">
+          <p className="price_txt">ЦEНА:</p>
+          <span className='number'>12 999 руб.</span>
+        </div>
+
+
+<div className="bord_p">
+  <i className="plus"></i>
+</div>
+      </div>
+      
+    </div>
+
+
+    <div className="card">
+
+      <img src={img1} alt="" className="card_img" />
+      <div className="bord"><i className="heart"></i></div>
+
+      <p className="tit_desc">Мужские Кроссовки Under Armour Curry 8</p>
+
+      <div className="price flex">
+        <div className="price_num">
+          <p className="price_txt">ЦEНА:</p>
+          <span className='number'>12 999 руб.</span>
+        </div>
+
+
+<div className="bord_p">
+  <i className="plus"></i>
+</div>
+      </div>
+      
+    </div>
+
   </div>
 	</div>
 	</section>
