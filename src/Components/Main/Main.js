11a57c6ff6a3a2d68d94dc3e4fbef6dda8370b03
mir312 <dakievameerim0@gmail.com>
2024-11-26 21:38:57 +0600

    created sec main

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -5,6 +5,11 @@ import './Main.css'
     return(
     <section className ="title">
 	<div className="container">
+    <div className="sec_tit flex">
+ <h2 className="tit_txt">Все кроссовки</h2>
+
+ <input type="text" className="input"  placeholder='Поиск'/>
+    </div>
 	
 	</div>
 	</section>
